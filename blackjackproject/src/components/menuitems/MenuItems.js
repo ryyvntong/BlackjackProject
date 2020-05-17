9bbb120717d70eda8eec715b9cb939b0a7cc6856
ryyvntong <rem.water@hotmail.com>
2020-05-17 14:47:20 -0400

    buttonfix2

--- blackjackproject/src/components/menuitems/MenuItems.js
+++ blackjackproject/src/components/menuitems/MenuItems.js
@@ -5,7 +5,8 @@ const menuItems=(props)=>{
     return(
         <div>
             <MenuItem clicked={props.clickedRules} icon="fas fa-book">Rules</MenuItem>
-            <MenuItem clicked={props.clickedSettings} icon="fas fa-cog">Settings</MenuItem>         
+            <MenuItem clicked={props.clickedSettings} icon="fas fa-cog">Settings</MenuItem>      
+            <p>*** When pressing the deal button for the first time, please give some time for the API to load up.</p>   
         </div>
         
     )
--- src/components/menuitems/MenuItems.js
+++ src/components/menuitems/MenuItems.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import MenuItem from './menuitem/MenuItem'
+import classes from './MenuItems.module.css'
 
 const menuItems=(props)=>{
     return(
         <div>
             <MenuItem clicked={props.clickedRules} icon="fas fa-book">Rules</MenuItem>
-            <MenuItem clicked={props.clickedSettings} icon="fas fa-cog">Settings</MenuItem>         
+            <MenuItem clicked={props.clickedSettings} icon="fas fa-cog">Settings</MenuItem>      
+            <p className={classes.disclaimer}>*** When pressing the deal button for the first time, please give some time for the API to load up. It takes around 20 seconds to initialize.</p>     
         </div>
         
     )
